5fc4977c565b617e3f3bd9d01edb005d0b5893e0
alokshakya <alokshakyacode@gmail.com>
2017-04-07 19:50:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var crypto = require('crypto');
 var Pool = require('pg').Pool;
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var config = {
   host: 'db.imad.hasura-app.io',
   port: '5432',
@@ -17,6 +18,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret:'someRandomSecretValue',
+    Cookie: { maxAge: 1000*60*60*24*30}
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
