3eb8dadca14484e15a077dbb2f8db9e9c4f7d08e
alokshakya <alokshakyacode@gmail.com>
2017-04-07 19:02:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,33 @@
-console.log('Loaded!');
+
+//submit username and password to login
+var submit=document.getElementById('sub_btn');
+submit.onclick = function() {
+    // Create a request Object
+    var request= new XMLHttpRequest();
+    // Capture the response from the server and store it in a variable
+    request.onreadystatechange=function(){
+        if(requst.readyState===XMLHttpRequest.DONE){
+            //Take some action when XMLHttpRequst is completed
+            if(requst.status===200){
+                //TODO
+                console.log('user logged succesfully');
+                alert("logged in successfully");
+            }
+            else if(request.status===403){
+                alert('user/password wrong!');
+            }
+            else if(request.status===500){
+                alert('something went wrong on server');
+            }
+        }
+        
+    };
+    var username = Document.getElementById('username');
+    var password = Document.getElementById('password');
+    //for devugging purpose print username and password in console
+    console.log('username');
+    console.log('password');
+    request.open('POST', 'http://alokshakya.imad.hasura-app.io/login', true);
+    request.send(JSON.stringify({username: username, password: password}));
+    
+};
