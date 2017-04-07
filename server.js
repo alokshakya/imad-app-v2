2f6c0141f823ef3504a89e890cf8a15bd7512cf3
alokshakya <alokshakyacode@gmail.com>
2017-04-07 17:11:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
 var Pool = require('pg').Pool;
+var bodyParser = require('body-parser');
 var config = {
   host: 'db.imad.hasura-app.io',
   port: '5432',
@@ -15,6 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -44,6 +46,24 @@ app.get('/hash/:input', function(req,res){
     var salt ='this-is-random-salt';
     var hashedString = hash(req.params.input,salt);
     res.send(hashedString);
+});
+
+//create user function
+app.post('/create-user', function(req,res){
+    // fetch user name and password from body
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString=hash(password,salt); // creating hash value from password
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1,$2)',[username, dbString], function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send("user succesfully created :"+username);
+        }
+    });
+    
 });
 // getting articles information with no SQL injection
 app.get('/articles/:articleName', function(req, res){
