525e1b06a62d29995d2ef13ed2d58d6daa811039
alokshakya <alokshakyacode@gmail.com>
2017-04-07 16:34:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ function hash(input, salt){
     return hashed.toString('hex');
 }
 app.get('/hash/:input', function(req,res){
-    //var salt ='this-is-random-salt';
-    var hashedString = hash(req.params.input,'this-is-random-salt');
+    var salt ='this-is-random-salt';
+    var hashedString = hash(req.params.input,salt);
     res.send(hashedString);
 });
 // getting articles information with no SQL injection
