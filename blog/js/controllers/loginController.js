3b86df4366efc556fc991246f3027e0e627a196e
alokshakya <alokshakyacode@gmail.com>
2017-04-09 00:15:05 +0530

    [imad-console] Updates blog/js/controllers/loginController.js

--- blog/js/controllers/loginController.js
+++ blog/js/controllers/loginController.js
@@ -2,10 +2,12 @@ app.controller("loginController", function ($scope, $http, $templateCache) {
      $scope.user={"name":"", "password":""};
     
 
-     $scope.fetch = function() {
+     $scope.loginUser = function() {
       $scope.code = null;
       $scope.response = null;
       var data1=$scope.user;
+      console.log('username :'+data1.name);
+      console.log('password :'+data1.password);
 
       $http({method: 'POST', url: '/loginuser',data: { name: data1.name, password: data1.password }, cache: $templateCache}).
         then(function(response) {
