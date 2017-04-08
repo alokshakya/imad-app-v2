f09850ab8708dd1e7dab19e66d75b767c9a1860d
alokshakya <alokshakyacode@gmail.com>
2017-04-09 00:12:50 +0530

    [imad-console] Updates blog/js/controllers/loginController.js

--- blog/js/controllers/loginController.js
+++ blog/js/controllers/loginController.js
@@ -1,38 +1,19 @@
-app.controller("loginController", function ($scope, $http) {
+app.controller("loginController", function ($scope, $http, $templateCache) {
      $scope.user={"name":"", "password":""};
     
 
-        $scope.loginUser = function () {
-            var data = $scope.user;
-           console.log('username :'+ data.name);
-           console.log('username :'+ data.password);
-            // Create a request Object
-    var request= new XMLHttpRequest();
-    // Capture the response from the server and store it in a variable
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            //Take some action when XMLHttpRequst is completed
-            if(request.status===200){
-                //TODO
-                console.log('user created succusfully');
-                $scope.res=data;
-                alert("logged in successfully");
-            }
-            else if(request.status===403){
-                alert('user/password wrong!');
-            }
-            else if(request.status===500){
-                alert('something went wrong on server');
-            }
-            else if(request.status===503){
-                alert('password is wrong!');
-            }
-        }
-        
-    };
-    request.open('POST', 'http://alokshakya.imad.hasura-app.io/loginuser', true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({name: data.name, password: data.password}));
+     $scope.fetch = function() {
+      $scope.code = null;
+      $scope.response = null;
+      var data1=$scope.user;
 
-        };
+      $http({method: 'POST', url: '/loginuser',data: { name: data1.name, password: data1.password }, cache: $templateCache}).
+        then(function(response) {
+          $scope.status = response.status;
+          $scope.data = response.data;
+        }, function(response) {
+          $scope.data = response.data || 'Request failed';
+          $scope.status = response.status;
+      });
+    };
 });
