8b7526a0168269a7ba5fcb36445f728f8fb00cdd
alokshakya <alokshakyacode@gmail.com>
2017-04-10 03:53:29 +0530

    [imad-console] Updates blog/js/controllers/createCommentController.js

--- blog/js/controllers/createCommentController.js
+++ blog/js/controllers/createCommentController.js
@@ -17,11 +17,7 @@ app.controller("createCommentController", function ($scope, $http, $templateCach
           $scope.data = response.data || 'Request failed';
           $scope.status = response.status;
       });
-      if(reaponse.status===200){
-          $scope.info1=response.data[response.data.length-1];
-          $('#'+$scope.info.id).removeClass('hidden');
-          
-      }
+      
     };
       
 });
