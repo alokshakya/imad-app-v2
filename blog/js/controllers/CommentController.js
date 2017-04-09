75abe31a0a9a8e2d3b005dec45da4efee027b35f
alokshakya <alokshakyacode@gmail.com>
2017-04-09 17:20:52 +0530

    [imad-console] Updates blog/js/controllers/CommentController.js

--- blog/js/controllers/CommentController.js
+++ blog/js/controllers/CommentController.js
@@ -17,5 +17,21 @@ app.controller("CommentController", function ($scope, $http, $templateCache) {
           $scope.status = response.status;
       });
     };
+    $scope.createComment = function() {
+      $scope.code = null;
+      $scope.response = null;
+      var article_id=$scope.info.id;
+      console.log('article_id is :'+article_id);
+
+      $http({method: 'POST', url: '/comments',data: { article_id: article_id }, cache: $templateCache}).
+        then(function(response) {
+          $scope.status = response.status;
+          $scope.comments = response.data;
+          fetchComment();
+        }, function(response) {
+          $scope.data = response.data || 'Request failed';
+          $scope.status = response.status;
+      });
+    };
       
 });
