973e4fd8c4acf9b8276bb28b0369a301efa288de
alokshakya <alokshakyacode@gmail.com>
2017-04-09 11:43:27 +0530

    [imad-console] Updates blog/js/controllers/createArticleController.js

--- blog/js/controllers/createArticleController.js
+++ blog/js/controllers/createArticleController.js
@@ -5,8 +5,9 @@ app.controller("createArticleController", function ($scope, $http, $templateCach
       $scope.code = null;
       $scope.response = null;
       var data1=$scope.article;
-      console.log('username :'+data1.name);
-      console.log('password :'+data1.password);
+      console.log('title :'+data1.title);
+      console.log('category :'+data1.category);
+      console.log('content :'+ data1.content);
 
       $http({method: 'POST', url: '/createArticle',data: { title: data1.title, category: data1.category, content: data1.content }, cache: $templateCache}).
         then(function(response) {
