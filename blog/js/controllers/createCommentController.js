13ba0ecd788d9ff1613db4c8d6884b79865d1f41
alokshakya <alokshakyacode@gmail.com>
2017-04-10 01:55:30 +0530

    [imad-console] Updates blog/js/controllers/createCommentController.js

--- blog/js/controllers/createCommentController.js
+++ blog/js/controllers/createCommentController.js
@@ -7,8 +7,9 @@ app.controller("createCommentController", function ($scope, $http, $templateCach
       var data1=$scope.comment;
       console.log('article id :'+$scope.info.id);
       console.log('content :'+ data1);
+      console.log('user is : '+$scope.info.name);
 
-      $http({method: 'POST', url: '/createComment',data: { article_id: $scope.info.id, comment: data1 }, cache: $templateCache}).
+      $http({method: 'POST', url: '/createComment',data: { article_id: $scope.info.id, comment: data1, name:$scope.info.name }, cache: $templateCache}).
         then(function(response) {
           $scope.status = response.status;
           $scope.data = response.data;
