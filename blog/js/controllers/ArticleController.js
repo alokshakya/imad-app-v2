1810a0981a788e05315353208939f233a8dcc1e8
alokshakya <alokshakyacode@gmail.com>
2017-04-09 14:45:09 +0530

    [imad-console] Updates blog/js/controllers/ArticleController.js

--- blog/js/controllers/ArticleController.js
+++ blog/js/controllers/ArticleController.js
@@ -1,4 +1,4 @@
-app.controller('ArticleController', ['$scope',$http,$templateCache, function($scope, $http, $templateCache) {
+app.controller('ArticleController', ['$scope','$http','$templateCache', function($scope, $http, $templateCache) {
 	$scope.articles=[{"id":"100","title":"cheking","content":"new content","category":"category","date":"2017-02-12"}];
  	$http({method: 'GET', url: '/articles', cache: $templateCache}).
         then(function(response) {
