edceb6487bb16c4268a5993dc9da06a3fdc4e50b
alokshakya <alokshakyacode@gmail.com>
2017-04-09 20:23:39 +0530

    [imad-console] Updates blog/js/controllers/createCommentController.js

--- blog/js/controllers/createCommentController.js
+++ blog/js/controllers/createCommentController.js
@@ -1,14 +1,14 @@
 app.controller("createCommentController", function ($scope, $http, $templateCache) {
-     $scope.comment="";
+     $scope.comment={"comment":""};
      console.log('comment outside function :'+$scope.comment);
       $scope.createCommentOnServer = function() {
       $scope.code = null;
       $scope.response = null;
-      var data1=$scope.article;
+      var data1=$scope.comment;
       console.log('article id :'+$scope.info.id);
-      console.log('comment is :'+$scope.comment);
+      console.log('comment is :'+data1.comment);
 
-      $http({method: 'POST', url: '/createComment',data: { article_id: $scope.info.id, comment: $scope.comment }, cache: $templateCache}).
+      $http({method: 'POST', url: '/createComment',data: { article_id: $scope.info.id, comment: data1.comment }, cache: $templateCache}).
         then(function(response) {
           $scope.status = response.status;
           $scope.data = response.data;
