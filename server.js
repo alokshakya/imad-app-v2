ff3e31f95a5fc7338156641d55821b820c612150
alokshakya <alokshakyacode@gmail.com>
2017-04-09 12:38:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -293,6 +293,18 @@ app.post('/createArticle', function(req,res){
     }
     
 });
+app.get('/articles', function(req, res){
+    // database functioning cheking
+    pool.query("SELECT * FROM articles ", function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+    
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
