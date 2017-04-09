2b3addbe98271b309ff6eab0c085d8cf7388a3c3
alokshakya <alokshakyacode@gmail.com>
2017-04-09 20:00:39 +0530

    Create createCommentController.js

---
+++ blog/js/controllers/createCommentController.js
@@ -0,0 +1,21 @@
+app.controller("createCommentController", function ($scope, $http, $templateCache) {
+     $scope.comment="";
+    
+      $scope.createCommentOnServer = function() {
+      $scope.code = null;
+      $scope.response = null;
+      var data1=$scope.article;
+      console.log('article id :'+$scope.info.id);
+      console.log('comment is :'+$scope.comment);
+
+      $http({method: 'POST', url: '/createComment',data: { article_id: $scope.info.id, comment: $scope.comment }, cache: $templateCache}).
+        then(function(response) {
+          $scope.status = response.status;
+          $scope.data = response.data;
+        }, function(response) {
+          $scope.data = response.data || 'Request failed';
+          $scope.status = response.status;
+      });
+    };
+      
+});
