21135569b0bc41b7922ad374b2837ae5a4735d4d
alokshakya <alokshakyacode@gmail.com>
2017-04-11 01:55:08 +0530

    [imad-console] Updates blog/js/app.js

--- blog/js/app.js
+++ blog/js/app.js
@@ -1,21 +1,21 @@
 var app=angular.module("myApp", ['infinite-scroll']);
 app.controller('DemoController', function($scope, $http) {
   $scope.articles=[];
-  $scope.busy= 'false';
+  $scope.busy= {"busy":false};
   var con= false;
   $scope.after=10;
   console.log('outside nextPage() after is :'+$scope.after);
-  console.log('outside nextPage() busy is :'+$scope.busy);
+  console.log('outside nextPage() busy is :'+$scope.busy.busy);
 
     $scope.nextPage = function($scope,$http) {
        // console.log('inside nextPage() busy is :'+$scope.busy);
-       // console.log('inside nextPage() article is :'+$scope.busy);
-    //if ($scope.busy) return;
-   // $scope.busy = 'true';
+        console.log('inside nextPage() article is :'+$scope.busy.busy);
+    if ($scope.busy.busy) return;
+    $scope.busy.busy = true;
     
  //make a post request to get article with data of id article id
       console.log('Inside nextPage() after is :'+$scope.after);
-      console.log('value of busy is before requset :'+$scope.busy);
+      console.log('value of busy is before requset :'+$scope.busy.busy);
       var article_id=$scope.after;
       console.log('article_id is :'+article_id);
 
@@ -24,7 +24,7 @@ app.controller('DemoController', function($scope, $http) {
           $scope.status = response.status;
           $scope.articles.push(response.data);
           $scope.after=1+$scope.after;
-         // $scope.busy='false';
+          $scope.busy.busy=false;
         }, function(response) {
           $scope.data = response.data || 'Request failed';
           $scope.status = response.status;
@@ -32,7 +32,7 @@ app.controller('DemoController', function($scope, $http) {
  //end of poat request with id
    
   };
-  console.log('value of busy is after requset is completed :'+$scope.busy);
+  console.log('value of busy is after requset is completed :'+$scope.busy.busy);
 
 });
 
