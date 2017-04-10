e49d131203b0d2e41321b66526c412737b619e14
alokshakya <alokshakyacode@gmail.com>
2017-04-10 12:46:33 +0530

    [imad-console] Updates blog/js/app.js

--- blog/js/app.js
+++ blog/js/app.js
@@ -1,4 +1,4 @@
-var app=angular.module("myApp", ['infinite-scroll']);
+var app=angular.module("myApp", ['infinite-scroll','ngResource']);
 app.controller('DemoController', function($scope, Reddit) {
   $scope.reddit = new Reddit();
 });
