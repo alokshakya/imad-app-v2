c9f91fd6240c4fb6735b283b446b3229bf51a62a
alokshakya <alokshakyacode@gmail.com>
2017-04-11 01:48:58 +0530

    [imad-console] Updates blog/js/app.js

--- blog/js/app.js
+++ blog/js/app.js
@@ -1,16 +1,17 @@
 var app=angular.module("myApp", ['infinite-scroll']);
 app.controller('DemoController', function($scope, $http) {
   $scope.articles=[];
-  $scope.busy= false;
+  $scope.busy= 'false';
   var con= false;
   $scope.after=10;
   console.log('outside nextPage() after is :'+$scope.after);
   console.log('outside nextPage() busy is :'+$scope.busy);
 
     $scope.nextPage = function($scope,$http) {
-        console.log('inside nextPage() busy is :'+$scope.busy);
-    if ($scope.busy) return;
-    $scope.busy = true;
+       // console.log('inside nextPage() busy is :'+$scope.busy);
+        console.log('inside nextPage() article is :'+$scope.busy);
+    //if ($scope.busy) return;
+   // $scope.busy = 'true';
     
  //make a post request to get article with data of id article id
       console.log('Inside nextPage() after is :'+$scope.after);
@@ -23,7 +24,7 @@ app.controller('DemoController', function($scope, $http) {
           $scope.status = response.status;
           $scope.articles.push(response.data);
           $scope.after=1+$scope.after;
-          $scope.busy=false;
+         // $scope.busy='false';
         }, function(response) {
           $scope.data = response.data || 'Request failed';
           $scope.status = response.status;
