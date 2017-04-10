cd490bf2f38c896cb878d0a1373d1b1f0dfdc74a
alokshakya <alokshakyacode@gmail.com>
2017-04-11 02:20:19 +0530

    [imad-console] Updates blog/js/app.js

--- blog/js/app.js
+++ blog/js/app.js
@@ -4,26 +4,28 @@ app.controller('DemoController', function($scope, Reddit) {
 });
 
 // Reddit constructor function to encapsulate HTTP and pagination logic
-app.factory('Reddit', function($http) {
+app.factory('Reddit', function($http,$scope) {
   var Reddit = function() {
-    this.items = [];
-    this.busy = false;
-    this.after = '';
+    $scope.articles = [];
+    $scope.busy = false;
+    $scope.after = '';
   };
 
   Reddit.prototype.nextPage = function() {
     if (this.busy) return;
     this.busy = true;
 
-    var url = "https://api.reddit.com/hot?after=" + this.after + "&jsonp=JSON_CALLBACK";
-    $http.jsonp(url).success(function(data) {
-      var items = data.data.children;
-      for (var i = 0; i < items.length; i++) {
-        this.items.push(items[i].data);
-      }
-      this.after = "t3_" + this.items[this.items.length - 1].id;
-      this.busy = false;
-    }.bind(this));
+    var url = "http://alokshakya.imad.hasura-app.io/articles/" + this.after +"";
+    $http({method: 'GET', url: '/article/'+article_id+''}).
+        then(function(response) {
+          $scope.status = response.status;
+          $scope.articles.push(response.data);
+          $scope.after=1+$scope.after;
+          
+        }, function(response) {
+          $scope.data = response.data || 'Request failed';
+          $scope.status = response.status;
+      }).bind(this);
   };
 
   return Reddit;
