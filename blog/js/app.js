83d00a4cac0b023404c991e6b7db7c37aabe01e3
alokshakya <alokshakyacode@gmail.com>
2017-04-11 11:29:04 +0530

    [imad-console] Updates blog/js/app.js

--- blog/js/app.js
+++ blog/js/app.js
@@ -1,36 +1,26 @@
-var app= angular.module('myApp', ['infinite-scroll']);
-app.controller('DemoController',['$scope','$http', function($scope, $http) {
-  $scope.articles=[];
-  $scope.busy= false;
+app.controller('NextPageController',['$scope','$http',functioin($scope,$http){
+	$scope.currentPage=10;
+	$scope.totalPage=100;
+	$scope.articles=[];
 
-  $scope.after=10;
-  console.log('outside nextPage() after is :'+$scope.after);
-  console.log('outside nextPage() busy is :'+$scope.busy);
+	//define function to get article by page number
+	function GetArticleData(page){
+		$scope.busy=true;
+		
 
-    $scope.nextPage = function($scope,$http) {
-       //console.log('inside nextPage() busy is :'+$scope.busy);
-       
-  
-    
- //make a post request to get article with data of id article id
-      console.log('Inside nextPage() after is :'+$scope.after);
-      
-      var article_id=$scope.after;
-      console.log('article_id is :'+article_id);
+	}
+	//end of defined function
 
-      $http({method: 'GET', url: '/article/'+article_id+''}).
-        then(function(response) {
-          $scope.status = response.status;
-          $scope.articles.push(response.data);
-          $scope.after=1+$scope.after;
-          
-        }, function(response) {
-          $scope.data = response.data || 'Request failed';
-          $scope.status = response.status;
-      });
- //end of poat request with id
-   
-  };
-  console.log('value of busy is after requset is completed :'+$scope.busy);
+	// call function for first time to get first article on page
+	GetArticleData($scope.currentPage);
+
+	//now implement nextPage() function
+	$scope.nextPage = function{
+		if($scope.busy===true) return;
+		$scope.currentPage+=1;
+		GetArticleData($scope.currentPage);
+
+	};
+	//end of next page function
 
 }]);
