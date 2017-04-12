d02c1053b5781b97f700340e3b2066789bc36827
alokshakya <alokshakyacode@gmail.com>
2017-04-12 13:08:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ app.get('/articles/:article_id', function(req, res){
     var end = parseInt(start)+3;
     console.log('start id :'+start);
     console.log('end id :'+end);
-    pool.query("SELECT * FROM articles WHERE id=$1",[start], function(err, result){
+    pool.query("SELECT * FROM articles WHERE id>=$1 AND id<$2",[start,end], function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }
